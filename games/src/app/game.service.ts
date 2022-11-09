d98bcf0f5a0caec6a13043d5e4118fc0a0128458
Ali Abbiss <aliabbiss@rocketmortgage.com>
2022-11-09 15:21:39 -0500

    games practice started

---
+++ games/src/app/game.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Game } from './game';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GameService {
+  TheList: Game[] = [
+    {title: "g1", year: 1, console: false}
+    {title: "g2", year: 2, console: true}
+  ];
+
+  constructor() { }
+}
